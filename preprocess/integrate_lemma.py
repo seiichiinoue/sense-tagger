3b9013ab9b0d90551ec12876ccfc2769b42325f9
seiichi <seiichi@aldebaran.sd.tmu.ac.jp>
2023-02-06 10:00:53 +0900

    relocated files

---
+++ preprocess/integrate_lemma.py
@@ -0,0 +1,23 @@
+import json
+
+usages_path = "data/usages.json"
+output_path = "data/usages_integrated.json"
+
+with open(usages_path, "r") as f:
+    usages = json.load(f)
+
+usages_new = {}
+for lemma_pos, vals in usages.items():
+    if lemma_pos == "noun_grouping_template":
+        continue
+    if len(lemma_pos.split("-")) > 2:  # in the case of phrase like `short-circuit-v`
+        continue
+    lemma, pos = lemma_pos.split("-")
+    if lemma not in usages_new.keys():
+        usages_new[lemma] = {}
+    for sense_num, definition_usages in vals.items():
+        pos_sense_num = pos + sense_num
+        usages_new[lemma][pos_sense_num] = definition_usages
+
+with open(output_path, "w") as f:
+    json.dump(usages_new, f, indent=4)
